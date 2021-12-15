27b2e8899d84814333d17b1f09daf65698de9997
Janio Isacura <janioisacura@gmail.com>
2021-12-15 17:04:08 -0300

    New app added

---
+++ 04-gif-expert-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import GifExpertApp from "./GifExpertApp";
+import { Container, Row } from "react-bootstrap";
+
+ReactDOM.render(
+  <Container>
+    <Row>
+      <GifExpertApp />
+    </Row>
+  </Container>,
+  document.getElementById('root')
+);
