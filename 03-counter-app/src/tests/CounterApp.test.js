533b1dc37c164a6d40d9f6d2f8bc1ebd5a40627a
Janio Isacura <janioisacura@gmail.com>
2021-12-15 00:06:08 -0300

    Counter app click simulation in progress

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -1,15 +1,26 @@
 import "@testing-library/jest-dom";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import CounterApp from "../CounterApp";
+import Button from "react-bootstrap/Button";
 
 describe('Pruebas en <CounterApp />', () => {
+  const wrapper = shallow(<CounterApp />);
   test('debe mostrar <CounterApp /> correctamente ', () => {
-    const wrapper = shallow(<CounterApp />);
+    
     expect(wrapper).toMatchSnapshot();
   });
   test('debe mostrar el valor por defecto', () => {
     const wrapper = shallow(<CounterApp  value={100}/>);
     const counterText = wrapper.find('p > span.Code').text();
     expect(counterText).toBe('100');
+  });
+  test('debe de incrementar con el botón de +1', () => {
+    const btn = mount(<Button />)
+    const wrapper = mount(<CounterApp />);
+    const btn1 = btn.find("button").at(0);
+    console.log(btn1.html());
+    const counterText = wrapper.find("p > span.Code").text();
+    btn1.simulate('click');
+    console.log(counterText);
   });
 });
