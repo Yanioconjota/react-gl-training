f35e73ed5daed64b982e2d07d34b7f6ae2955ff2
Janio Isacura <janioisacura@gmail.com>
2022-01-31 10:43:29 -0300

    Style update

--- 00-rick-and-morty/src/components/MainApp.js
+++ 00-rick-and-morty/src/components/MainApp.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col, Card } from 'react-bootstrap'
 import { useGetData } from "../hooks/useGetData";
 import '../assets/scss/MainApp.scss';
+import { classNames } from "../helpers/classNames";
 
 const MainApp = () => {
 
@@ -18,15 +19,22 @@ const MainApp = () => {
         {!loading &&
           characters.map((ch) => (
             <Col xs={12} md={3} key={ch.id}>
-              <Card>
+              <div className="card">
                 <img alt={ch.name} src={ch.image} />
-                <Card.Body>
+                <div className="card-body">
                   <p><strong>Name</strong>: {ch.name}</p>
                   <p><strong>Species</strong>: {ch.species}</p>
                   <p><strong>Origin</strong>: {ch.origin}</p>
-                  <p><strong>Status</strong>: {ch.status}</p>
-                </Card.Body>
-              </Card>
+                  <p><strong>Status</strong>: 
+                    <div className={classNames({
+                      'badge bg-success' : ch.status === 'Alive',
+                      'badge bg-danger' : ch.status === 'Dead',
+                      'badge bg-dark' : ch.status === 'unknown'
+                    })
+                    }> {ch.status}</div>
+                  </p>
+                </div>
+              </div>
             </Col>
           ))}
       </Row>
