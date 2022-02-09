c11022f055642bb2b1836975b3a2b3a5cad94e5e
Janio Isacura <janioisacura@gmail.com>
2022-02-09 19:12:09 -0300

    HeroCard html structure update

--- 07-heroes-app/src/components/heroes/HeroCard.js
+++ 07-heroes-app/src/components/heroes/HeroCard.js
@@ -4,32 +4,24 @@ const HeroCard = ({
   id,
   superhero,
   publisher,
-  alter_ego,
-  first_appearance,
-  characters
+  alter_ego
 }) => {
 
   const imagePath = `./assets/img/${id}.jpg`;
 
   return (
-    <div className="col-12 col-md">
+    <div className="col-6 col-md-3 col-lg-2">
       <div className="card">
         <div className="row no-gutters">
-          <div className="col-4">
-            <img src={imagePath} className="card-img" alt={superhero} />
-          </div>
-          <div className="col-8">
-            <div className="card-body">
-              <h5 className="card-title">{superhero}</h5>
-              <p className="card-text"><strong>Name: </strong>{alter_ego}</p>
-              {/* {
-                (alter_ego !== characters) &&
-                <p><strong>Portrayed by:</strong> {characters}</p>
-              } */}
-              {/* <p><strong>First appearance:</strong> {first_appearance}</p> */}
-              <p><strong>Publisher:</strong> {publisher}</p>
-              <Link className="text-info" to={`/hero/${id}`}>Mas info...</Link>
-            </div>
+          <img src={imagePath} className="card-img-top" alt={superhero} />
+          <div className="card-body px-4">
+            <h2 className="card-title">{superhero}</h2>
+            <p>
+              {publisher}
+            </p>
+            <Link className="btn w-100 mt-3 btn-info" to={`/hero/${id}`}>
+              Mas info...
+            </Link>
           </div>
         </div>
       </div>
