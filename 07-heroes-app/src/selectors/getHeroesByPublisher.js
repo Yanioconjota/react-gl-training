5c7865607489e83faaf1eb0969e324b2fcf16cf2
Janio Isacura <janioisacura@gmail.com>
2022-02-11 12:42:26 -0300

    heroes are now displayed in alphabetical order

--- 07-heroes-app/src/selectors/getHeroesByPublisher.js
+++ 07-heroes-app/src/selectors/getHeroesByPublisher.js
@@ -1,8 +1,18 @@
 import { heroes } from "../data/heroes";
 
 export const getHeroesByPublisher = (publisher) => {
+
+  heroes.sort((a, b) => {
+    if (a.superhero < b.superhero) {
+      return -1;
+    }
+    if (a.superhero > b.superhero) {
+      return 1;
+    }
+    return 0;
+  });
   
-  const validPublishers = ['DC Comics', 'Marvel Comics'];
+  const validPublishers = ['DC Comics', 'Marvel Comics', 'all'];
 
   if (!validPublishers.includes(publisher)) {
     throw new Error(`${publisher} is not a valid publisher`);
