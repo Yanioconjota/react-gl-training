9edd3cef3c47981cb51bbbc799ff5f3d73506c5a
Janio Isacura <janioisacura@gmail.com>
2022-02-21 16:38:47 -0300

    AppRouter UT update

--- 07-heroes-app/src/tests/routers/AppRouter.test.js
+++ 07-heroes-app/src/tests/routers/AppRouter.test.js
@@ -4,14 +4,12 @@ import { AuthContext } from "../../auth/authContext";
 import AppRouter from "../../routers/AppRouter";
 
 describe('pruebas con AppRouter', () => {
-
-  const contextValue = {
-    user: {
-      logged: false,
-    },
-  };
-
   test('debe mostrar el login si el usuario no está autenticado', () => {
+    const contextValue = {
+      user: {
+        logged: false,
+      },
+    };
     const wrapper = mount(
       <AuthContext.Provider value={ contextValue }>
         <AppRouter/>
@@ -19,6 +17,22 @@ describe('pruebas con AppRouter', () => {
     console.log(wrapper.html());
     expect(wrapper).toMatchSnapshot();
     expect(wrapper.find('h1').text().trim()).toBe('Login');
+  });
+
+  test('debe mostrar el componente Marvel si el usuario está autenticado', () => {
+    const contextValue = {
+      user: {
+        logged: true,
+        name: 'Homero'
+      },
+    };
+    const wrapper = mount(
+      <AuthContext.Provider value={ contextValue }>
+        <AppRouter/>
+      </AuthContext.Provider>);
+    //console.log(wrapper.html());
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.navbar').exists()).toBe(true);
   });
 
 });
