79470e93f4cebaa4386a4ae797b73e36eba147a9
Janio Isacura <janioisacura@gmail.com>
2022-03-10 18:57:15 -0300

    New react project created

---
+++ 08-journal-app/src/routers/AppRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { JournalScreen } from '../components/journal/JournalScreen';
+import { AuthRouter } from './AuthRouter';
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/auth" component={AuthRouter}></Route>
+        <Route path="/" exact component={JournalScreen}></Route>
+      </Switch>
+    </Router>
+  );
+}
