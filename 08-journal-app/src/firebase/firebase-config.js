578527dfed0e4031bbc2c79db192289a91f1d4de
Janio Isacura <janioisacura@gmail.com>
2022-03-15 10:44:55 -0300

    firebase config file added

---
+++ 08-journal-app/src/firebase/firebase-config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4GVCMhYov_WXfhPeOWK_C-S9bzmlb6_4",
+  authDomain: "react-app-cursos-c1ab7.firebaseapp.com",
+  projectId: "react-app-cursos-c1ab7",
+  storageBucket: "react-app-cursos-c1ab7.appspot.com",
+  messagingSenderId: "514107416359",
+  appId: "1:514107416359:web:cfb9b8b30a8d8d3f9a9302",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export {
+  db,
+  googleAuthProvider,
+  firebase
+};
