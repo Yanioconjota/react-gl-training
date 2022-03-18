617291c3b725ecd627ab8eb1339684deeb04e47b
Janio Isacura <janioisacura@gmail.com>
2022-03-18 16:55:23 -0300

    notes added to types

--- 08-journal-app/src/types/types.js
+++ 08-journal-app/src/types/types.js
@@ -1,10 +1,20 @@
+//El nombre entre corchetes representa al reducer encargado de esa acción
+
 export const types = {
   login: "[Auth] Login",
   logout: "[Auth] Logout",
 
   uiSetError: "[UI] Set Error",
   uiRemoveError: "[UI] Remove Error",
-  
-  uiStartLoading: '[UI] Start loading',
-  uiFinishLoading: '[UI] Finish loading'
+
+  uiStartLoading: "[UI] Start loading",
+  uiFinishLoading: "[UI] Finish loading",
+
+  notesAddNew: "[Notes] New note",
+  notesActive: "[Notes] Set active note",
+  notesLoad: "[Notes] Load notes",
+  notesUpdated: "[Notes] Updated note",
+  notesFileUrl: "[Notes] Updated image url",
+  notesDelete: "[Notes] Delete note",
+  notesLogoutCleaning: "[Notes] Logout Cleaning",
 };
