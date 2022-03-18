3188764e5c3735f6b17765416c2d8ef3b5a2cc87
Janio Isacura <janioisacura@gmail.com>
2022-03-18 16:56:25 -0300

    New event listener added on sidebar to handle new notes action

--- 08-journal-app/src/components/journal/Sidebar.js
+++ 08-journal-app/src/components/journal/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { startLogout } from '../../actions/auth';
+import { startNewNote } from '../../actions/notes';
 import { JournalEntries } from './JournalEntries';
 
 export const Sidebar = () => {
@@ -13,6 +14,10 @@ export const Sidebar = () => {
 
   //Toma el name del state almacenado en la app
   const { name } = useSelector( state => state.auth );
+
+  const handleAddNew = () => {
+    dispatch(startNewNote());
+  }
 
   return (
     <aside className="journal__sidebar">
@@ -27,7 +32,7 @@ export const Sidebar = () => {
         </button>
       </div>
 
-      <div className="journal__new-entry">
+      <div className="journal__new-entry" onClick={ handleAddNew }>
         <i className="far fa-calendar-plus fa-5x"></i>
         <p className="mt-5">New entry</p>
       </div>
