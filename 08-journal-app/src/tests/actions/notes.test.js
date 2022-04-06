d5c780391f9ab89220096554e04da2c409840e7d
Janio Isacura <janioisacura@gmail.com>
2022-04-06 10:43:14 -0300

    payload tests update and startLoadingNotes action test added

--- 08-journal-app/src/tests/actions/notes.test.js
+++ 08-journal-app/src/tests/actions/notes.test.js
@@ -1,30 +1,51 @@
+/**
+ * @jest-environment node
+ */
+
+// para evitar el error: FIRESTORE (9.6.8) INTERNAL ASSERTION FAILED: Unexpected state debe agregarse el comentario anterior en la primera línea
+
 //*2-importaciones para el mockStore
 import configureStore from "redux-mock-store";
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 //-fin imports del mockStore
-import { db } from '../../firebase/firebase-config';
+import { db } from "../../firebase/firebase-config";
 
-import { startNewNote } from "../../actions/notes";
+import {
+  startNewNote,
+  activeNote,
+  addNewNote,
+  setNotes,
+  startLoadingNotes,
+} from "../../actions/notes";
 import { types } from "../../types/types";
 
 //*3- config del muckStore
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-//hacemos el mock del estado del store actual
-const store = mockStore({
+//creamos un estado inicial
+const initState = {
   auth: {
-    uid: 'testingUID'
-  }
-});
+    uid: "testingUID",
+  },
+};
+
+//hacemos el mock del estado del store actual
+let store = mockStore(initState);
 
 describe("Pruebas con notes actions", () => {
+
+  beforeEach(() => {
+    //regresamos el store al estado inicial antes de iniciar cada test
+    store = mockStore(initState);
+  });
+
   //El método startNewNote accede al store para obtener el estado con getState y hace un dispatch de activeNote y addNewNote, previamente debemos hacer una configuración:
-  
+
   //1- instalar redux-mock-store: npm install redux-mock-store --save-dev
   //2- hacemos las importaciones cecesarias
   //3- configurar el store
-  test("debe crear una nueva nota con startNewNote", async() => {
+  test("debe crear una nueva nota con startNewNote", async () => {
     //FirebaseError: 7 PERMISSION_DENIED: Missing or insufficient permissions.
     //Necesitamos estar "autenticados" por ende crearemos una db de testing en firebase y agregamos la configuración a nuestro archivo src/firebase/firebase-config.js
 
@@ -60,5 +81,69 @@ describe("Pruebas con notes actions", () => {
     await db.doc(`testingUID/journal/notes/${actions[0].payload.id}`).delete();
 
     //no repetimos el delete porque es el mismo ID para ambas acciones
+  });
+
+  test("prueba de los payload de addNewNote, activeNote, setNote", () => {
+    const noteMock = {
+      title: "Title",
+      body: "some random text",
+      date: new Date().getTime(),
+    };
+
+    const docId = "mockID123";
+
+    const addNewNoteMock = addNewNote(docId, noteMock);
+    const activeNoteMock = activeNote(docId, noteMock);
+    const setNotesMock = setNotes([noteMock]);
+
+    expect(addNewNoteMock).toEqual({
+      type: types.notesAddNew,
+      payload: {
+        id: docId,
+        ...noteMock,
+      },
+    });
+
+    expect(activeNoteMock).toEqual({
+      type: types.notesActive,
+      payload: {
+        id: docId,
+        ...noteMock,
+      },
+    });
+
+    expect(setNotesMock).toEqual({
+      type: types.notesLoad,
+      payload: [noteMock]
+    });
+  });
+
+  test("startLoadingNotes debe cargar las notas", async () => {
+
+    //Al igual que en la primera prueba usamos el dispatch para disparar la acción
+    await store.dispatch(startLoadingNotes("testingUID"));
+    //las obtenemos con el getActions
+    const actions = store.getActions();
+
+    console.log(actions);
+    //Esperamos que tenga la siguiente estructura
+    expect(actions[0]).toEqual({
+      type: types.notesLoad,
+      payload: expect.any(Array)
+    });
+
+    //Creamos un "modelo" de la nota esperada para el próximo expect
+    //Si no viene la url no hay problema porque solo se evaluaran las siguientes propiedades
+    const expectedNote = {
+      id: expect.any(String),
+      title: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(Number),
+    };
+
+    console.log(actions[0].payload[0]);
+
+    expect(actions[0].payload[0]).toMatchObject(expectedNote);
+
   });
 });
